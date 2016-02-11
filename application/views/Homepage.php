33b727a9d7d97ea9b9cf393d8d62a0ab162d81f4
sivanalkalay <sivanalkalay@msn.com>
2016-02-11 12:54:02 -0800

    Changed homepage html Changed some css for homepage

--- application/views/Homepage.php
+++ application/views/Homepage.php
@@ -13,13 +13,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-		<p>This is the base homepage m8</p>
+	<div id="gameStatus">
+		<p>Game Status:</p>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	
 	</div>
+        
+        <div id="players">
+		<p>List of Players:</p>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+		
+	</div>
+        
 </div>
 
 </body>
