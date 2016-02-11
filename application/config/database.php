55132f01504e319c8e10bb7c92bf5f44891f1767
mentalslug <victor.van1000@gmail.com>
2016-02-11 12:57:42 -0800

    Made a lot of basefiles work, every page now loads and we have a base controller Signed-off-by:mentalslug <victor.van1000@gmail.com>

--- application/config/database.php
+++ application/config/database.php
@@ -71,23 +71,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | the query builder class.
 */
 $active_group = 'default';
-$query_builder = TRUE;
+$active_record = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'username' => 'root',
+	'password' => 'squeal',
+	'database' => 'botcards3',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
+	'pconnect' => True,
+	'db_debug' => True,
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
 	'dbcollat' => 'utf8_general_ci',
 	'swap_pre' => '',
+        'autoinit' => TRUE,
 	'encrypt' => FALSE,
 	'compress' => FALSE,
 	'stricton' => FALSE,
