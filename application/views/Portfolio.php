584560b4a40493eb4baa7f13082088fbfe783ccd
sivanalkalay <sivanalkalay@msn.com>
2016-02-12 14:25:16 -0800

    made the portfolio page

--- application/views/Portfolio.php
+++ application/views/Portfolio.php
@@ -1,8 +1,3 @@
 	<h1>Welcome to portfolio!</h1>
-
-	<div id="body">
-		<p>This is the base homepage m8</p>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
+        {playerportfolio}
+        {dropdown}
